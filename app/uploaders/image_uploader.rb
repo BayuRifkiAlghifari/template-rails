b9fbcba3d2103f26aa72a1f76141b748976fc43b
BayuRifkiAlghifari <bayurifkialgh@gmail.com>
2021-09-16 15:32:50 +0700

    fix: Image Uploader

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,17 +1,36 @@
 class ImageUploader < Shrine
+  IMAGE_TYPES = %w[image/jpg image/jpeg image/png image/webp image/svg]
+  VIDEO_TYPES = %w[video/mp4 video/quicktime]
+  PDF_TYPES   = %w[application/pdf]
+ 
   Attacher.validate do
-    validate_max_size 5*1024*1024, # 5 GB
-    validate_mime_type_inclusion ['image/jpg', 'image/jpeg', 'image/png', 'image/svg']
+    validate_max_size 5*1024*1024
+    validate_mime_type IMAGE_TYPES + VIDEO_TYPES + PDF_TYPES
   end
-
-  # Eager processing / derivatives processing
+ 
   Attacher.derivatives do |original|
+    case file.mime_type
+      when *IMAGE_TYPES then process_derivatives(:image, original)
+      when *VIDEO_TYPES then process_derivatives(:video, original)
+      when *PDF_TYPES   then process_derivatives(:pdf,   original)
+    end
+  end
+ 
+  Attacher.derivatives :image do |original|
     magick = ImageProcessing::MiniMagick.source(original).saver(quality: 90)
     {
       large:  magick.resize_to_limit!(800, 800),
       medium: magick.resize_to_limit!(500, 500),
       small:  magick.resize_to_limit!(300, 300),
     }
+  end
+ 
+  Attacher.derivatives :video do |original|
+    # ... 
+  end
+ 
+  Attacher.derivatives :pdf do |original|
+    # ... 
   end
 
   # Fallback to original
